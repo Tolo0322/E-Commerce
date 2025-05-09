8c7a439139e4f82cb2e8b5a3d6e22effbe8c9cd9
tolo garcia <tolo032286@gmail.com>
2025-05-08 20:47:45 -0500

    mostrando productos en productDetail

--- src/components/ProductDetail/index.jsx
+++ src/components/ProductDetail/index.jsx
@@ -1,16 +1,34 @@
 import { XMarkIcon } from '@heroicons/react/24/solid'
 
+import { useContext } from 'react';
+import { ShoppingCartContext } from '../../context/ShoppingCartContext';
 import "./styles.css";
 
 const ProductDetail = () => {
+    const context = useContext(ShoppingCartContext);
+    
+    
     return (
-        <aside className="product-detail flex flex-col fixed bg-white right-0 border border-black rounded-lg">
+        <aside className={ `${context.isProductDetailOpen ? 'flex' : 'hidden' } product-detail  flex-col fixed bg-white right-0 border border-black rounded-lg`}>
             <div className="flex justify-between items-center p-6">
                 <h2 className="font-medium">Detail</h2>
                 <div>
-                <XMarkIcon className="size-6 text-black" />
-                </div>
+                <XMarkIcon 
+                    className="size-6 text-black cursor-pointer" 
+                    onClick={() => context.closeProductDetail()} />
+                </div>                
             </div>
+            <figure className='px-6'>
+                    <img
+                        className='w-full h-full rounded-lg' 
+                        src={context.productToShow.image} 
+                        alt="context.productToShow.title" />
+            </figure>
+            <p className='flex flex-col p-6' >
+            <span className='font-medium text-2xl mb-2'>{context.productToShow.price}</span>
+            <span className='font-medium text-md'>{context.productToShow.title}</span>
+            <span className='font-light text-sm'>{context.productToShow.description}</span>
+            </p>
         </aside>
     )
 }
--- src/context/index.jsx
+++ src/context/index.jsx
@@ -4,13 +4,27 @@ import { ShoppingCartContext } from "./ShoppingCartContext";
 
 
 export const ShoppingCartProvider = ({children}) => {
+    //Shopping cart - Increment Quantity
     const [count, setCount] = useState(0)
+
+    //Product Detail - Open Close
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
+    const openProductDetail = () => setIsProductDetailOpen(true);
+    const closeProductDetail= () => setIsProductDetailOpen(false);
+
+    //Product Detail -  Show Product
+    const [productToShow, setProductToShow] = useState({});
 
 
     return (
         <ShoppingCartContext.Provider value={{
             count,
-            setCount
+            setCount,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen,
+            productToShow,
+            setProductToShow
         }}>
             {children}
         </ShoppingCartContext.Provider>
